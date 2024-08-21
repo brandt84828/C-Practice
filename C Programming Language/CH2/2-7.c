630c7ecca4c5d4fc61a6053f9ebe0a26d97c6225
brandt84828 <brandt84828@gmail.com>
2024-08-21 08:57:26 +0800

    practice

---
+++ C Programming Language/CH2/2-7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+unsigned invert(unsigned x, int p, int n);
+
+int main(void) { printf("%u", (unsigned) invert((unsigned) 32, (int) 3, (int) 3)); }
+
+unsigned invert(unsigned x, int p, int n) {
+    return x ^ (~(~0 << n) << (p + 1 - n));
+}
