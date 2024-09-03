2293aa9a23668f2ca9ae8689eddfb7009bcb1401
brandt84828 <brandt84828@gmail.com>
2024-09-03 14:37:30 +0800

    practice

---
+++ C Programming Language/CH4/4-2.c
@@ -0,0 +1,82 @@
+#include <ctype.h>
+#include <stdio.h>
+
+#define MAXLINE 100
+
+double myatof(char s[]);
+
+int mgetline(char line[], int lim);
+
+int main(void) {
+    char str[MAXLINE];
+    double num;
+    mgetline(str, MAXLINE);
+
+    num = myatof(str);
+    printf("%f", num);
+
+    return 0;
+}
+
+double myatof(char s[]) {
+    double val, pow;
+    int sign, i, esign, exp;
+    double power(double base, int exp);
+
+    for (i = 0; isspace(s[i]); i++);
+
+    sign = (s[i] == '-') ? -1 : 1;
+
+    if (s[i] == '+' || s[i] == '-')
+        i++;
+
+    for (val = 0.0; isdigit(s[i]); i++)
+        val = 10.0 * val + (s[i] - '0');
+
+    if (s[i] == '.')
+        i++;
+
+    for (pow = 1.0; isdigit(s[i]); i++) {
+        val = 10.0 * val + (s[i] - '0');
+        pow *= 10.0;
+    }
+
+    if (s[i] == 'e' || s[i] == 'E')
+        i++;
+    if (s[i] == '+' || s[i] == '-') {
+        esign = s[i];
+        i++;
+    }
+
+    for (exp = 0; isdigit(s[i]); i++)
+        exp = 10.0 * exp + (s[i] - '0');
+    
+    if (esign == '-')
+        return sign * (val / pow) / power(10.0, exp);
+    else
+        return sign * (val / pow) * power(10.0, exp);
+}
+
+int mgetline(char line[], int lim) {
+    int i, c;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        line[i] = c;
+
+    if (c == '\n')
+        line[i++] = c;
+
+    line[i] = '\0';
+}
+
+double power(double base, int exp) {
+    double power;
+    power = 1.0;
+    //printf("%f", exp);
+    while (exp-- > 0) {
+        //printf("%f\n", power);
+        power *= base;
+    }
+    //printf("%f\n", power);
+    return power;
+}
